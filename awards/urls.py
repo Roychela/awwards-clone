9385c8fc131f6b8d87afcaca5c83a614df2a3bb3
Roy Chela <roychela@gmail.com>
2019-08-05 01:32:09 +0300

    Add urls paths for profile, search and api view functions

--- awards/urls.py
+++ awards/urls.py
@@ -6,7 +6,13 @@ from . import views
 
 urlpatterns=[
      url(r'^$', views.index, name='welcome'),
-   
+     url(r'^accounts/profile/update', views.update_profile, name='profile'),
+     url(r'^accounts/profile', views.my_profile, name='myaccount'),
+     url(r'^user/(?P<user_id>\d+)$', views.user_profile, name='aboutuser'),
+     url(r'^upload/', views.upload_project, name='upload_project'),
+     url(r'^api/profiles/$', views.ProfileList.as_view()),
+     url(r'^api/projects/$', views.ProjectList.as_view()),
+     url(r'^search/', views.search_results, name='search_results'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
